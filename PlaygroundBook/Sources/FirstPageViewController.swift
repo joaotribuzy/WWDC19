9c9b670b34fc11fb07cb7484e4612e8af6255fab
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2019-03-20 13:50:29 -0400

    Ajustando arquivo exclusivo para a LiveView

---
+++ PlaygroundBook/Sources/FirstPageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FirstPageViewController.swift
+//  Book_Sources
+//
+//  Created by João Tribuzy on 20/03/19.
+//
+
+import UIKit
+
+class FirstPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var lab: UILabel!
+    
+    @IBAction func actonnnn(_ sender: Any) {
+        lab.text = "1213423423423114"
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
