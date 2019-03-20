b27e3c73f14e02f6e36bfd7de8e9c7308a1adcaf
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2019-03-20 11:04:52 -0400

    Template Inicial

--- PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/LiveView.swift
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/LiveView.swift
@@ -10,3 +10,5 @@ import PlaygroundSupport
 
 // Instantiate a new instance of the live view from the book's auxiliary sources and pass it to PlaygroundSupport.
 PlaygroundPage.current.liveView = instantiateLiveView()
+
+
