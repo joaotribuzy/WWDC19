d1ea97c751731d2da7f574cb2ecf7c5fd98dff0d
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2019-03-21 15:42:41 -0400

    Adicionando melodia1

--- PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
@@ -6,4 +6,13 @@
 //  The Swift file containing the source code edited by the user of this playground book.
 //
 //#-end-hidden-code
-let str = "Hello, playground"
+/*:
+ To help us playing bass on our rehearse you need to...play a **bass** ...😂😂😂,
+ **But let's start explaining th fundamentals!!**
+ 
+ On music, we have something called **melody**, wich is a combination of some musical notes, building a litle **musical idea**. Each **melody** have a single peculiarity, established by the music in which it is inserted.
+ ---
+ ### Change the melody
+ */
+public var melody = /*#-editable-code*/.melody1/*#-end-editable-code*/
+
